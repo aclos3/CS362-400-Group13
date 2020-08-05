da0731863ced4d128bf9de1d07f1965ec644c1c8
aclos3 <45724446+aclos3@users.noreply.github.com>
2020-08-05 12:38:55 -0700

    func_1: Decimal Handling
    Can handle float numbers containing a single decimal point.

--- task.py
+++ task.py
@@ -9,16 +9,35 @@
 def conv_num(num_str):
     # convert num_str to list of ASCII characters
     asc_arr = []
+    has_dec = 0
+    aft_dec = 0
     actual_num = 0
+    
     for asc_char in num_str:
         asc_arr.append(ord(asc_char))
+        # check to see if charcter is a decimal
+        if(ord(asc_char) == 46):
+            has_dec += 1
 
+    #print(asc_arr[0])
+    #print(asc_arr[1])
+    #print(asc_arr[2])
     # assemble a number from the array of characters
     for x in range(0, len(asc_arr)):
+        multiplier = len(asc_arr) - x - 1
+        # check to see if decimal is present, set flag
+        if(asc_arr[x] == 46):
+            aft_dec = x + 1
+        
+        # digits occurring after decimal
+        elif(aft_dec):
+            actual_num += (1 / (10 ** (x - aft_dec + 1)))  * (asc_arr[x] - 48)
+            #print("adding: " + str((1 / (10 ** (x - aft_dec + 1)))  * (asc_arr[x] - 48)))
+        # digits occurring before decimal
+        else:
+            actual_num += 10 ** (multiplier - (has_dec * 2)) * (asc_arr[x] - 48)
 
-        len(asc_arr) - x
-        actual_num += (10 ** (len(asc_arr) - x - 1)) * (asc_arr[x] - 48)
-
+    
     return actual_num
 
 
