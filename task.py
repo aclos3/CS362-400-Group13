d8dd1240dac0db86739965a0f4c5c82bc18301f3
Dylan Bergschneider <40582165+dcbergs@users.noreply.github.com>
2020-08-05 10:23:57 -0700

    skeleton functions added for all three

--- task.py
+++ task.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ##############################################################################
 #  Group 13
 #
@@ -6,3 +7,18 @@
 
 def my_func():
     return "Hello World"
+=======
+def conv_num(num_str):
+    actual_number = 0
+    return actual_number
+
+
+def my_datetime(num_sec):
+    date_string = ''
+    return date_string
+
+
+def conv_endian(num, endian = 'big'):
+    endian_string = ''
+    return endian_string
+>>>>>>> Stashed changes
--- tests.py
+++ tests.py
@@ -1,7 +1,24 @@
 import unittest
+from task import conv_num, my_datetime, conv_endian
 
 
+<<<<<<< Updated upstream
 class TestCase(unittest.TestCase):
+=======
+class TestConvNum(unittest.TestCase):
+
+    def test1(self):
+        self.assertTrue(True)
+
+
+class TestDateTime(unittest.TestCase):
+
+    def test1(self):
+        self.assertTrue(True)
+
+
+class TestEndian(unittest.TestCase):
+>>>>>>> Stashed changes
 
     def test1(self):
         self.assertTrue(True)
