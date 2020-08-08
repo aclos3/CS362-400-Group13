fd1c7d1d280a217a8e23c7157d80d9989b6eeaec
Dylan Bergschneider <40582165+dcbergs@users.noreply.github.com>
2020-08-07 18:56:37 -0700

    handle negatives, zero, and little-endianness

--- tests.py
+++ tests.py
@@ -31,6 +31,26 @@ class TestEndian(unittest.TestCase):
         endian = conv_endian(95478689)
         self.assertEqual(endian, '05 B0 E3 A1')
 
+    def test_positive_little_1(self):
+        endian = conv_endian(4987673, 'little')
+        self.assertEqual(endian, '19 1B 4C')
+
+    def test_positive_little_2(self):
+        endian = conv_endian(12, 'little')
+        self.assertEqual(endian, '0C')
+
+    def test_negative_big_1(self):
+        endian = conv_endian(-954786, 'big')
+        self.assertEqual(endian, '-0E 91 A2')
+
+    def test_negative_little_1(self):
+        endian = conv_endian(-87458, 'little')
+        self.assertEqual(endian, '-A2 55 01')
+
+    def test_zero(self):
+        endian = conv_endian(0)
+        self.assertEqual(endian, '00')
+
 
 if __name__ == '__main__':
     unittest.main()
